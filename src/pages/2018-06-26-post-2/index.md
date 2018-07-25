78dc93ba3c8c808920720bb62807577b0cca7860
Brayn <ilogirb23@yahoo.com>
2018-07-25 17:38:12 +0800

    initial commit

---
+++ src/pages/2018-06-26-post-2/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-2"
+date: "2018-06-26"
+title: "My First Gatsby Post"
+author: "Brayn Brigoli"
+---
+
+This is my second blog post in Gatsby.
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to my website</h1>
+    <p>Sample site to Gatsby Crash Course.</p>
+    <p>Now go build something great.</p>
+  </div>
+)
+
+export default IndexPage
